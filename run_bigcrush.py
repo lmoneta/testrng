84da5abdad88716dbcdf2075f057e3f4b1c09107
Lorenzo Moneta <Lorenzo.Moneta@cern.ch>
2018-12-14 15:14:04 +0100

    add script to run many tests

---
+++ run_bigcrush.py
@@ -0,0 +1,39 @@
+##
+
+# run all biig crush tests
+
+nproc = 8
+njobs = 10
+option = "-rabbit"
+rng_name = "ranluxpp"
+outPrefix = "/home/moneta/cernbox/root/tests/mixmax/"
+
+import os
+import sys
+import multiprocessing as mp
+
+def func(pos,n) :
+    print "Running for process",pos
+    for i in range(0,n):
+#        option = " -rabbit"
+#        gname = " mixmax17"
+        cmd = "./testu01_bigcrush " + option + " " + rng_name
+        rid = pos * njobs + i + 1
+        outLogFile = outPrefix + "/rabbit/test_2E30/test_" + rng_name + ".run" + str(rid) + ".log"
+        cmd += " > " + outLogFile
+        print "running",cmd
+        ret = os.system(cmd)
+        if (ret != 0) :
+            sys.exit()
+
+
+processes = [mp.Process(target=func, args=(x,njobs) )  for x in range(nproc)]
+
+# Run processes
+for p in processes:
+    p.start()
+
+# Exit the completed processes
+for p in processes:
+    p.join()
+
